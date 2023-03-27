ccbf675cb7701cae7ead3476281223af160ffd7a
Leon simintei <mallfs@icloud.com>
2023-03-27 08:35:58 +0300

    routes added

--- app/controllers/heros_controller.rb
+++ app/controllers/heros_controller.rb
@@ -1,2 +1,22 @@
 class HerosController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
+    def index
+        render json: Hero.all 
+    end
+
+    def show
+        hero = find_hero
+        render json: hero, serializer: HeroWithPowersSerializer
+    end
+
+
+    private
+
+    def find_hero
+        Hero.find(params[:id])
+    end
+
+    def record_not_found_response(exception)
+        render json: { error: "Hero not found" }, status: :not_found
+    end
 end
