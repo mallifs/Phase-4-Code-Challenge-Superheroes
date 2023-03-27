679ae4137ea7cef8af3222f81f60cd16bc109421
Leon simintei <mallfs@icloud.com>
2023-03-27 10:46:13 +0300

    controllers added

--- app/controllers/hero_powers_controller.rb
+++ app/controllers/hero_powers_controller.rb
@@ -1,2 +1,25 @@
 class HeroPowersController < ApplicationController
+    def create
+        hero = Hero.find_by(id: params[:hero_id])
+        power = Power.find_by(id: params[:power_id])
+
+        if hero && power
+            hero_power = HeroPower.new(hero_power_params)
+            hero_power.hero = hero
+            hero_power.power = power
+            if hero_power.save
+                render json: hero, serializer: HeroWithPowersSerializer
+            else
+                render json: { errors: hero_power.errors.full_messages }, status: :unprocessable_entity
+            end
+        else
+            render json: { error: "Hero or Power not found" }, status: :not_found
+        end 
+    end
+
+    private
+
+    def hero_power_params
+        params.require(:hero_power).permit(:strength)
+    end
 end
--- app/controllers/powers_controller.rb
+++ app/controllers/powers_controller.rb
@@ -1,2 +1,40 @@
 class PowersController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :record_not_found_response
+
+    def index
+        render json: Power.all
+    end
+
+    def show
+        power = find_power
+        render json: power
+    end
+
+    def update
+        power =  find_power
+        if power
+          if power.update(power_params)
+            render json: power
+          else
+            render json: { errors: power.errors.full_messages }, status: :unprocessable_entity
+          end
+        else
+          render json: { error: "Power not found" }, status: :not_found
+        end
+      end
+
+    private
+
+    def find_power
+        Power.find(params[:id])
+    end
+
+    def power_params
+        params.require(:power).permit(:description)
+    end
+
+    def record_not_found_response(exception)
+        render json: { error: "Power not found" }, status: :not_found
+    end
+
 end
